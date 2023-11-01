2dc4cfe4fdad75f87ec9dec34577f82807bbfada
BrandonAWong <brandon.alex.wong@gmail.com>
2023-11-01 00:13:07 -0700

    map: finish map

--- Dungeons and Monsters/map.py
+++ Dungeons and Monsters/map.py
@@ -7,7 +7,7 @@ class Map:
             cls._instance = super().__new__(cls)
         return cls._instance
     
-    def __init__(self, ):
+    def __init__(self):
         if not Map._initialized:
             with open("map.txt", "r") as f:
                 self._map: list[list[chr]] = f.readlines()
@@ -16,3 +16,24 @@ class Map:
 
     def __getitem__(self, row: int) -> list[chr]:
         return self._map[row]
+
+    def __len__(self):
+        '''Returns number of rows in the map'''
+        return len(self._map)
+
+    def show_map(self, loc: list[int]) -> str:
+        '''
+        Returns the map as a string and displays revelead locations.
+        Unrevealed locations will be denoted with an "x".
+        '''
+        mapped: list[list[str]] = [[self[i][j] if self._revealed[i][j] or self[i][j] == "\n" else "x" for j in range(len(self[i]))] for i in range(len(self))]
+        mapped[loc[0]][loc[1]] = "*"
+        return "".join([" ".join(row) for row in mapped])
+    
+    def reveal(self, loc: list[int]) -> None:
+        '''Sets input location as seen'''
+        self._revealed[loc[0]][loc[1]] = True
+
+    def remove_at_loc(self, loc: list[int]) -> None:
+        '''Sets last position seen by Hero to "n"'''
+        self[loc[0]][loc[1]] = "n"
