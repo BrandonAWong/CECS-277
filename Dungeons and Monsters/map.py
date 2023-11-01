d9e658beb777ece8e6ceb33698b5daab6e9195b1
BrandonAWong <brandonwong2004@outlook.com>
2023-10-31 22:22:30 -0700

    map: singleton logic

--- Dungeons and Monsters/map.py
+++ Dungeons and Monsters/map.py
@@ -1,3 +1,18 @@
 class Map:
-    def __new__(self, cls):
-        
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args): 
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self, ):
+        if not Map._initialized:
+            with open("map.txt", "r") as f:
+                self._map: list[list[chr]] = f.readlines()
+            self._revealed: list[list[bool]] = [[False for _ in row] for row in self._map]
+            Map._initialized = True
+
+    def __getitem__(self, row: int) -> list[chr]:
+        return self._map[row]
