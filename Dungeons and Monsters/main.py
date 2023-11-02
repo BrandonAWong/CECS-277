c424e4684c839fd3cc3f6af4aa2ce112d52b70ed
BrandonAWong <brandonwong2004@outlook.com>
2023-11-01 19:29:37 -0700

    main: randomly displaces player after running from enemy

--- Dungeons and Monsters/main.py
+++ Dungeons and Monsters/main.py
@@ -39,24 +39,24 @@ def main():
                     print(monster.attack(player))
                 elif battle_choice == 2:
                     dungeon.reveal(player.loc)
-                    print("You ran away!")
-                    while choice((player.go_north(), player.go_south(), player.go_east(), player.go_west())) == "o":
+                    print("You ran away!\n")
+                    while choice([player.go_north, player.go_south, player.go_east, player.go_west])() == "o":
                         continue
                     break
             if not monster.hp:
                 dungeon.remove_at_loc(player.loc)
-                print(f"You have slain a {monster.name}")
+                print(f"You have slain a {monster.name}\n")
         elif tile == "i":
             if player.hp < 25:
                 player.heal()
                 dungeon.remove_at_loc(player.loc)
-                print("You found a Health Potion! You drink it to restore your health.")
+                print("You found a Health Potion! You drink it to restore your health.\n")
             else:
-                print("You are at full health. You decide to save the potion for later.")
+                print("You are at full health. You decide to save the potion for later.\n")
         elif tile == "n":
-            print("There is nothing here...")
+            print("There is nothing here...\n")
         elif tile == "s":
-            print("You headed back to the start of the dungeon.")
+            print("You headed back to the start of the dungeon.\n")
         elif tile == "o":
             print("You cannot go that way...\n")
         elif tile == "f":
